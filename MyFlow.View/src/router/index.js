710f0a862353c0223ff83b648f74a24e5c13c612
rocky wong <42104700+rockyweng75@users.noreply.github.com>
2022-12-23 13:57:19 +0800

    route 改用webhash

--- MyFlow.View/src/router/index.js
+++ MyFlow.View/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router"
 import constantRoutes from "./constantRoutes"
 // import asyncRoutes from "./asyncRoutes"
 
 const base = import.meta.env.VITE_BASE
 const router = createRouter({
-    history: createWebHistory(base),
+    history: createWebHashHistory(base),
     routes: [...constantRoutes ]
 })
 
