c09bca099009e1165f1b5a7afd9282e1ca289226
rockyweng75 <wengsc@yuntech.edu.tw>
2022-12-23 22:01:00 +0800

    add transfer

---
+++ MyFlow.Service/Actions/Transfer/GenericTransfer.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyFlow.Domain.Models;
+using MyFlow.Service.Deadlines;
+using MyFlow.Service.Targets;
+using MyFlow.Service.Impl;
+using MyFlow.Domain.Enums;
+
+namespace MyFlow.Service.Actions.Submit
+{
+    public abstract class GenericTransfer : GenericAction, ITransfer
+    {
+        private IServiceProvider serviceProvider;
+        private IStageRouteService stageRouteService;
+        private IStageService stageService;
+
+        public GenericTransfer(
+            IServiceProvider serviceProvider,
+            ILogger<GenericSubmit> logger,
+            IApplyDataService applyDataService,
+            IApproveDataService approveDataService,
+            IJobLogService jobLogService,
+            IStageService stageService,
+            IStageRouteService stageRouteService
+        ) : base(
+                serviceProvider, 
+                logger, 
+                applyDataService, 
+                approveDataService,
+                jobLogService
+            )
+        {
+            this.serviceProvider = serviceProvider;
+            this.stageService = stageService;
+            this.stageRouteService = stageRouteService;
+        }
+
+        public abstract string Name { get; }
+        public abstract string Key { get; }
+
+        // public async Task<IList<StageVM>> FindNextStages(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData)
+        // {
+        //     return await FindNextStages(ActionType.送出, flowchart, currentStage, applyData, approveData);
+        // }
+
+        public async Task<string> FindStageTarget(StageVM stage, FlowchartVM flowchart, ApplyDataVM applyData, ApproveDataVM? approveData) 
+        {
+            var targetServices = serviceProvider.GetServices<ITarget>();
+            if(targetServices == null || targetServices.Count() == 0) throw new Exception($"找不到對應的Target: {stage.Target}");
+
+            var service = targetServices.Where(o => o.GetType().Name == stage.Target).FirstOrDefault();
+            var result = service != null ?
+                            await service.Invoke(stage, applyData, approveData) :
+                            throw new Exception($"找不到對應的Target: {stage.Target}");
+            return result;
+        }
+
+        public async Task<DateTime?> GetStageDeadline(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData)
+        {
+            var className = currentStage.Deadline;
+            if(string.IsNullOrEmpty(currentStage.Deadline)) className = "AnyTime";
+            var deadlines = serviceProvider.GetServices<IDeadline>();
+            if(deadlines == null || deadlines.Count() == 0) throw new Exception($"找不到對應的Deadline: {currentStage.Deadline}");
+            var deadline = deadlines.Where(o => o.GetType().Name == className).FirstOrDefault();
+            var result = deadline != null ?
+                            await deadline.GetEndDateTime(DateTime.Now.Year, flowchart, currentStage, applyData, approveData) :
+                            throw new Exception($"找不到對應的Deadline: {currentStage.Deadline}");
+            return result;
+        }
+
+        public async Task<ApproveDataVM> InitNextStageData(FlowchartVM flowchart, StageVM currentStage, StageVM nextStage, ApplyDataVM applyData, ApproveDataVM? approveData = null)
+        {
+            var closeDate = await GetStageDeadline(flowchart, currentStage, applyData, approveData);
+
+            var nextUser = await FindStageTarget(nextStage, flowchart, applyData, approveData);
+
+            if (approveData == null) 
+            {
+                approveData = new ApproveDataVM()
+                {
+                    ApplyId = applyData.Id,
+                    StageId = nextStage.Id,
+                    FlowId = flowchart.Id,
+                    StageName = nextStage.StageName,
+                    CloseDate = closeDate,
+                    FlowName = flowchart.FlowName,
+                    UserId = nextUser,
+                    StatusCode = null
+                };
+            };
+
+            return approveData;
+        }
+
+        public async Task Invoke(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData = null)
+        {
+            await NextAction(flowchart, currentStage, applyData, approveData);
+        }
+
+        public abstract Task NextAction(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData);
+    }
+}
---
+++ MyFlow.Service/Actions/Transfer/ITransfer.cs
@@ -0,0 +1,18 @@
+﻿using MyFlow.Domain.Models;
+
+namespace MyFlow.Service.Actions.Submit
+{
+    public interface ITransfer : IAction
+    {
+        // Task<IList<StageVM>> FindNextStages(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData);
+
+        Task<string> FindStageTarget(StageVM stage, FlowchartVM flowchart, ApplyDataVM applyData, ApproveDataVM? approveData);
+
+        Task<DateTime?> GetStageDeadline(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData);
+
+        Task<ApproveDataVM> InitNextStageData(FlowchartVM flowchart, StageVM currentStage, StageVM nextStage, ApplyDataVM applyData, ApproveDataVM? approveData = null);
+
+        Task NextAction(FlowchartVM flowchart, StageVM currentStage, ApplyDataVM applyData, ApproveDataVM? approveData);
+
+    }
+}
