2947b87e1ee3d33adcba72a0a3168bee01de7f4e
rockyweng75 <wengsc@yuntech.edu.tw>
2023-01-07 11:22:16 +0800

    modify chat.vue style

--- MyFlow.View/src/views/home/member/chat.vue
+++ MyFlow.View/src/views/home/member/chat.vue
@@ -2,39 +2,86 @@
     <el-drawer v-model="drawer" 
         :size="drawerSize"
         :withHeader="false"
-        :destroy-on-close="true">
+        :destroy-on-close="true"
+        class="chat-drawer">
         <el-row>
             <el-button link @click="close" style="width: 30px">
                 <el-icon color="black"><ArrowLeftBold /></el-icon>
             </el-button>
             <h4>{{ state.Target }}</h4>
         </el-row>
-        <el-table class="chatList">
-
-        </el-table>
-        <el-divider />
-        <el-form :model="state" :ref="chatForm">
-            <el-form-item label="輸入訊息" 
-                        prop="Message"
-                        :label-width="80">
-                <div style="display:inline;width:100%">
-                    <ckeditor :editor="editor" v-model="state.Message" :config="editorConfig"></ckeditor>
+        <div class="chat-list">
+            <div class="user" :class="row.From === 'Target' ? 'remote': 'local'"  v-for="(row, index) in messageList" :key="index">
+                <div class="avatar">
+                    <el-avatar :icon="UserFilled" />
+                    <div class="name">{{row.Name}}</div>
                 </div>
-            </el-form-item>
-        </el-form>
+                <div class="message-box">{{ row.Message }}</div>
+            </div>
+        </div>
+        <el-row class="chat-inputs">
+            <el-col :span="3">
+                <el-button type="primary" link>
+                    <el-icon :size="28">
+                        <MoreFilled />
+                    </el-icon>
+                </el-button>
+            </el-col>
+            <el-col :span="3">
+                <el-upload
+                    v-model:file-list="fileList"
+                    action=""
+                    multiple
+                >
+                    <el-button type="primary" link>
+                        <el-icon :size="28">
+                            <Paperclip />
+                        </el-icon>
+                    </el-button>
+                </el-upload>
+            </el-col>
+            <el-col :span="3">
+                <el-button type="primary" link>
+                    <el-icon :size="28">
+                        <Camera />
+                    </el-icon>
+                </el-button>
+            </el-col>
+            <!-- <el-col :span="2">
+                <el-button type="primary" link>
+                    <el-icon :size="28">
+                        <Location />
+                    </el-icon>
+                </el-button>
+            </el-col> -->
+            <el-col :span="14">
+                <el-input v-model="state.Message">
+                    <template #append>
+                        <el-button disabled>
+                            <el-icon>
+                                <Select />
+                            </el-icon>
+                        </el-button>
+                    </template>
+                </el-input>
+            </el-col>
+        </el-row>
     </el-drawer>
 </template>
 <script>
     import { reactive, computed, ref, onBeforeUpdate, watch } from 'vue'
     import { useStore } from 'vuex'
-    import { ArrowLeftBold } from '@element-plus/icons-vue'
-    import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-
+    import { ArrowLeftBold, Paperclip, Camera, Location, MoreFilled, Select } from '@element-plus/icons-vue'
     export default({
         props:['modelValue', 'target'],
         emits:['update:modelValue'],
         components:{
-            ArrowLeftBold
+            ArrowLeftBold,
+            Paperclip,
+            Camera,
+            Location,
+            MoreFilled,
+            Select
         },
         setup(props, {emit}){
             const store = useStore()
@@ -49,6 +96,25 @@
                 } else {
                     return '50%'
                 }
+            })
+
+            const messageList = computed(() =>{
+                return [
+                    { 
+                        From: 'Target',
+                        Avatar: '',
+                        Name: 'You',
+                        Message: 'Hello~',
+                        Images: []
+                    },
+                    {
+                        From: 'Custom',
+                        Name: 'Me',
+                        Avatar: '',
+                        Message: 'Hi',
+                        Images: []
+                    }
+                ]
             })
 
             const close = () =>{
@@ -71,27 +137,101 @@
                 drawer,
                 close,
                 drawerSize,
-                ClassicEditor,
-                editor: ClassicEditor,
-                editorConfig: {
-                    // toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
-                    // heading: {
-                    //     options: [
-                    //         { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                    //         { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                    //         { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                    //     ]
-                    // }
-                }
+                messageList
             }
         }
     });
 </script>
-<style scoped>
-    .chatList{
-        background-color: black;
-        color: white;
-        border-radius: 1ch;
-        height: 350px;
+<style scoped lang="scss">
+    .chat-drawer{
+        background-color: var(--el-color-info-light-6);
     }
+    .chat-list{
+        background-color: var(--el-color-info-light-8);
+        /*color: white; */
+        border: solid 1px black;
+        border-radius: 1ch;
+        height: 400px;
+        padding: 7px 2px 7px 2px;
+        box-shadow: 0 0 10px #aaa;
+        overflow-y: auto;
+    }
+    .chat-inputs{
+        background-color: var(--el-color-info-light-8);
+        border: solid 1px black;
+        border-radius: 1ch;
+        margin-top: 1em;
+        padding-top: 12px;
+    }
+
+    .user {
+        display: flex;
+        align-items: flex-start;
+        margin-bottom: 20px;
+
+        .avatar {
+            width: 60px;
+            text-align: center;
+            flex-shrink: 0;
+        }
+
+        .message-box {
+            background-color: #aaa;
+            padding: 16px;
+            border-radius: 10px;
+            position: relative;
+        }
+    }
+ 
+    // .remote{
+    //     text-align: left;
+    // }
+
+    // .local{
+    //     text-align: right;
+    // }
+
+    .remote {
+        .message-box {
+            margin-left: 20px;
+            margin-right: 80px;
+            color: #eee;
+            background-color: #4179f1;
+            &::before {
+                border-right: 10px solid #4179f1;
+                left: -10px;
+            }
+        }
+    }
+
+    .local {
+        justify-content: flex-end;
+        .message-box {
+            margin-right: 20px;
+            margin-left: 80px;
+            order: -1;
+            background-color: #fff;
+            color: #333;
+            &::before {
+                border-left: 10px solid #fff;
+                right: -10px;
+            }
+        }
+    }
+
+    .remote,
+    .local {
+        & .message-box::before {
+            content: "";
+            position: absolute;
+            top: 20px;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+        }
+        .message-box {
+            font-weight: 300;
+            box-shadow: 0 0 10px #888;
+        }
+    }
+   
 </style>
